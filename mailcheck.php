fbfd4fea503db3db664b06b7ee135b471efefdc5
George Hazlewood <ghazlewood@users.noreply.github.com>
2013-11-15 10:19:50 +0000

    Removing the ability to drop messages into Maildirs

--- mailcheck.php
+++ mailcheck.php
@@ -60,16 +60,6 @@ if (mysql_num_rows($result) > 0) {
     if ($percentuse > LOWER_LIMIT) {
 
         if ($percentuse > UPPER_LIMIT) {
-          $message = compose_message($mailbox, $percentuse, $quota, true);
-
-          if (defined(DELIVER_TO_ADMIN) && detect_environment()!='HTTP') {
-            $admin_dir = MAILNAMES.DOMAIN."/".ACCOUNT."/Maildir/new/".$msgid;
-            add_message_to_maildir($admin_dir, $message);
-          }
-          if (defined(DELIVER_TO_USER) && detect_environment()!='HTTP') {
-            $user_dir = MAILNAMES.$mailbox['name']."/".$mailbox['mail_name']."/Maildir/new/".$msgid;
-            add_message_to_maildir($user_dir, $message);
-          }
           $full[] = array(
             'mailbox' => $mailbox['mail_name'].'@'.$mailbox['name'], 
             'quota'=>$quota['quotamb'], 
@@ -77,16 +67,6 @@ if (mysql_num_rows($result) > 0) {
           );
           $num_full++;
         } else {
-          $message = compose_message($mailbox, $percentuse, $quota, false);
-
-          if (defined(DELIVER_TO_ADMIN) && detect_environment()!='HTTP') {
-            $admin_dir=MAILNAMES.DOMAIN."/".ACCOUNT."/Maildir/new/".$msgid;
-            add_message_to_maildir($admin_dir, $message);
-          }
-          if (defined(DELIVER_TO_USER) && detect_environment()!='HTTP') {
-            $user_dir = MAILNAMES.$mailbox['name']."/".$mailbox['mail_name']."/Maildir/new/".$msgid;
-            add_message_to_maildir($user_dir, $message);
-          }
           // Find out if this is a mailbox which also has an active redirect and therefore might also
           // fill up without every being emptied
           $redirect_sql = "SELECT `address` FROM `mail_redir` WHERE `mn_id` = ".$mailbox['id']." LIMIT 1";
@@ -146,53 +126,10 @@ if (!empty($mess)) {
       mail(RECIPIENT, 'Mailbox Quota Summary: '.$num_over.' over '.LOWER_LIMIT.'% and '.$num_full.' full mailboxes', $mess);
     }
     //echo $message;
-  } else {
-    // print report to screen
-    include_once(PRODUCT_ROOT."/admin/plib/modules/pm.php");
-    if (!$session->chkLevel(IS_ADMIN)) {
-      pm_alert(pm_lmsg('__perm_denied').'You must be admin to use this page');
-      go_to_uplevel();
-    }
-    print nl2br($mess);
-    print "<table><tr><th>Mailbox</th><th>Size (MB)</th><th>Quota (MB)</th><th>Percent Usage</th></tr>";
-
-    $sort = $_GET['sort'];
-    $order = $_GET['order'];
-    if (empty($order)) $order = 'desc';
-    if (!empty($sort)) {
-      // Obtain a list of columns
-      foreach ($report as $key => $row) {
-        $email[] = $row['email'];
-        $size[] = $row['size'];
-        $percentuse[] = $row['percentuse'];
-        $quota[] = $row['quota'];
-      }
-      $sort = $$sort;
-      switch($order) {
-        case 'asc':
-          $sort_order = 'SORT_ASC';
-          break;
-        case 'desc':
-          $sort_order = 'SORT_DESC';
-          break;
-      }
-      array_multisort($sort, $sort_order, $report);
-    }
-    foreach ($report as $rep_row) {
-      $warning_class = ($rep_row['percentuse'] > LOWER_LIMIT ? 'red' : '');
-      if ($rep_row['domain'] != $previous_domain) {
-        print "<tr><td colspan='3'>".$rep_row['domain']."</td></tr>";
-      }
-      print "<tr class='".$warning_class."'><td>".$rep_row['email']."</td><td>".$rep_row['size']."</td><td>".$rep_row['quota']."</td><td>".$rep_row['percentuse']."%</td></tr>";
-      $previous_domain = $rep_row['domain'];
-    }
-    print "</table>";
-  }
+  } 
 
 }
 
 if (isset($e)) {
   echo $e;
 }
-
-?>
